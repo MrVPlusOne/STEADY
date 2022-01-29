28291d6448e061b555bfecd237634b9f6d84b599
Jiayi Wei <wjydzh1@gmail.com>
2022-01-29 12:46:06 -0600

    Record obs_noise experiment results.

--- scripts/experiments/obs_noise_variation.jl
+++ scripts/experiments/obs_noise_variation.jl
@@ -4,9 +4,9 @@ perf_list = []
 
 let ° = π / 180
 
-    for σ_bearing in [5°, 1°, 2.5°, 10°, 20°]
+    for σ_bearing in [1°] # [5°, 1°, 2.5°, 10°, 20°]
         # you can find the available args inside `train_models.jl`.
-        global script_args = (; gpu_id=1, σ_bearing, exp_name="σ_bearing=$(σ_bearing/°)°")
+        global script_args = (; gpu_id=1, σ_bearing, lr=1e-4/20, exp_name="small_lr, σ_bearing=$(σ_bearing/°)°")
         my_include("../train_models.jl")
         push!(perf_list, Main.perf)
 
