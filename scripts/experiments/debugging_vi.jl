23c693bfb2012d9bbcc7ab238fbdde63de4711b8
Jiayi Wei <wjydzh1@gmail.com>
2022-01-31 21:55:57 -0600

    Simplify VI architecture.

--- scripts/experiments/debugging_vi.jl
+++ scripts/experiments/debugging_vi.jl
@@ -3,11 +3,13 @@ my_include = include # to avoid mess up the VSCode linter
 
 global script_args = (;
     is_quick_test=false,
-    gpu_id=6,
+    gpu_id=1,
     use_fixed_variance=false,
     use_simple_obs_model=true,
     train_method=:VI,
-    max_trian_steps=10_000,
+    max_train_steps=10_000,
 )
 my_include("../train_models.jl")
+exp_info = (architecture="Simplified VI", scenario="Hovercraft", obs_model="Gaussian with velocity")
+println("Experiment setting: $exp_info")
 DataFrame([Main.perf]) |> display
