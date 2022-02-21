e3194b0747ae7189f4b1e97c85e861bf905cc48b
Jiayi Wei <wjydzh1@gmail.com>
2022-02-20 21:15:18 -0600

    Implement obs_w_ablation.

---
+++ scripts/experiments/obs_w_ablation.jl
@@ -0,0 +1,24 @@
+include("experiment_common.jl")
+
+let scenarios = [
+        SEDL.HovercraftScenario(),
+        SEDL.RealCarScenario("ut_automata"),
+        SEDL.RealCarScenario("alpha_truck"),
+    ]
+
+    out_dir = mkpath(joinpath("results/obs_w_ablation/"))
+
+    for sce in scenarios
+        local script_args = (;
+            scenario=sce,
+            train_method=:EM,
+            gpu_id=Main.GPU_ID,
+            use_obs_weight_schedule=false,
+        )
+
+        perfs = train_multiple_times(script_args, 3).test_performance
+        measure = map(SEDL.to_measurement, perfs)
+
+        write(joinpath(out_dir, string(sce)), string(measure))
+    end
+end
